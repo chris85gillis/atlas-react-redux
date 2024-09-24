80c8cd1a29df7fac0c339129fa44490ddd397f84
Chris Gillis <chris85gillis@gmail.com>
2024-09-24 16:44:09 -0500

    lost delete function for the list, now its back, yay

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,6 +3,9 @@ import { useSelector } from 'react-redux';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 import { RootState } from '../store'; // Ensure this points to your Redux state
+import DeleteListButton from './DeleteListButton';
+import { useDispatch } from 'react-redux';
+import { deleteList } from '../slices/listsSlice';
 
 interface ListProps {
   listId: string;
@@ -12,9 +15,15 @@ interface ListProps {
 const List: React.FC<ListProps> = ({ listId, title }) => {
   const cards = useSelector((state: RootState) => state.lists.cards); // Get all cards from Redux store
   const list = useSelector((state: RootState) => state.lists.lists.find(list => list.id === listId));
+  const dispatch = useDispatch(); // Import useDispatch from react-redux
+
+  const handleDeleteList = () => {
+    dispatch(deleteList(listId));
+  };
 
   return (
     <div className="group/list h-full min-w-96 p-4">
+      <DeleteListButton onClick={handleDeleteList} />
       <h3>{title}</h3>
       {list?.cardIds.map((cardId) => {
         const card = cards[cardId]; // Find the card by ID
